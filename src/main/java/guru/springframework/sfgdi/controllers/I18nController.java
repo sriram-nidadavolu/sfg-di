721b6f8bfb3a65e57d8dcf6e3e0d9ec559ca82eb
Sriram Nidadavolu <sriramnidadavolu@gmail.com>
2024-06-17 23:34:40 -0400

    Spring Profiles

---
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingsService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class I18nController {
+
+    GreetingsService greetingsService;
+    public I18nController(@Qualifier("i18nService")GreetingsService greetingsService) {
+        this.greetingsService = greetingsService;
+    }
+
+    public String sayHello() {
+        return greetingsService.sayGreeting();
+    }
+}
